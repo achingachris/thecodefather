843c79503ca7a5f35cd9e14a6ba3533c4bf440fc
itschrisagain <chris.achinga@student.moringaschool.com>
2022-10-23 01:23:03 +0300

    restructure

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,5 +1,4 @@
-import Head from 'next/head'
-
+import MetaTags from './MetaTags'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
@@ -12,25 +11,12 @@ const Layout = ({
 }) => {
   return (
     <>
-      <Head>
-        <meta property='og:site_name' content='ChrisDevCode' />
-        <title>{`${pageTitle}: by Chris Achinga`}</title>
-        <meta httpEquiv='Content-Type' content='text/html; charset=UTF-8' />
-        <meta charSet='UTF-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='description' content={pageDescription} />
-        <meta name='google' content='nositelinkssearchbox' />
-        <meta name='robots' content='all' />
-        <meta property='og:description' content={pageDescription} />
-        <meta
-          property='og:image:alt'
-          content={`Image for ${pageTitle} by Chris Achinga`}
-        />
-        <meta property='og:title' content={pageTitle} />
-        <meta property='og:type' content='article' />
-        <meta property='og:url' content={pageURL} />
-        <meta property='og:image' content={pageImage} />
-      </Head>
+      <MetaTags
+        pageImage={pageImage}
+        pageTitle={pageTitle}
+        pageDescription={pageDescription}
+        pageURL={pageURL}
+      />
       <Navbar />
       {children}
       <Footer />
