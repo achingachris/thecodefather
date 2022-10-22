7df9de3b5e317ff0c047ee62bbc25780fefc39f6
itschrisagain <chris.achinga@student.moringaschool.com>
2022-10-23 01:12:49 +0300

    meta-tags

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,9 +3,34 @@ import Head from 'next/head'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
-const Layout = ({ children }) => {
+const Layout = ({
+  children,
+  pageTitle,
+  pageDescription,
+  pageURL,
+  pageImage,
+}) => {
   return (
     <>
+      <Head>
+        <meta property='og:site_name' content='ChrisDevCode' />
+        <title>{`${pageTitle}: by Chris Achinga`}</title>
+        <meta httpEquiv='Content-Type' content='text/html; charset=UTF-8' />
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='description' content={pageDescription} />
+        <meta name='google' content='nositelinkssearchbox' />
+        <meta name='robots' content='all' />
+        <meta property='og:description' content={pageDescription} />
+        <meta
+          property='og:image:alt'
+          content={`Image for ${pageTitle} by Chris Achinga`}
+        />
+        <meta property='og:title' content={pageTitle} />
+        <meta property='og:type' content='article' />
+        <meta property='og:url' content={pageURL} />
+        <meta property='og:image' content={pageImage} />
+      </Head>
       <Navbar />
       {children}
       <Footer />
