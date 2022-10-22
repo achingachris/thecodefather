c44d17ade6609d8d218eb680a541374e9ae0cb79
itschrisagain <chris.achinga@student.moringaschool.com>
2022-10-23 01:26:09 +0300

    added bootstrapjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js')
+  }, [])
   return <Component {...pageProps} />
 }
 
