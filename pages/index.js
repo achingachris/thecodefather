3374d2e8584b8b292b0af301d90e7f6e2838b790
itschrisagain <chris.achinga@student.moringaschool.com>
2022-10-23 13:36:26 +0300

    article list layout

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Layout from '../components/layout/Layout'
+import Header from '../components/home/Header'
+import ArticleList from '../components/shared/ArticleList'
+
 const HomePage = () => {
-  return <Layout>index</Layout>
+  return (
+    <Layout>
+      <Header />
+      <ArticleList />
+    </Layout>
+  )
 }
 
 export default HomePage
