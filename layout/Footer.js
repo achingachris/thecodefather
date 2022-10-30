a72a93aa944ab574d837dcec57a917239208b53d
itschrisagain <chris.achinga@student.moringaschool.com>
2022-10-31 00:39:44 +0300

    add year and foote content

--- layout/Footer.js
+++ layout/Footer.js
@@ -1,10 +1,21 @@
 const Footer = () => {
+  // get the current year
+  const year = new Date().getFullYear()
   return (
     <footer className='py-3 bg-dark'>
       <div className='container px-4 px-lg-5'>
         <p className='m-0 text-center text-white'>
-          Copyright © Your Website 2022
+          Copyright © Chris Achinga {year} | Deployed on Netlify
+          <a href='https://app.netlify.com/sites/codefather/deploys'>
+            <picture>
+              <img
+                alt='Netlify Status'
+                src='https://api.netlify.com/api/v1/badges/6a769aea-36f4-45f3-a317-166ca9adcb2c/deploy-status'
+              />
+            </picture>
+          </a>
         </p>
+        <p></p>
       </div>
     </footer>
   )
