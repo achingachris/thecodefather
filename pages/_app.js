f143f75fa92302ad5450e63c7553433f92eac8f0
itschrisagain <chris.achinga@student.moringaschool.com>
2022-11-19 01:43:21 +0300

    little bit of fixes here and there

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,34 @@
 import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  // enable google analytics track each page
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  // tag => G-W21XGX4BNB
+  // load bootstrap javascript files
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap')
   }, [])
+
   return (
     <>
       <Component {...pageProps} />
+      
     </>
   )
 }
