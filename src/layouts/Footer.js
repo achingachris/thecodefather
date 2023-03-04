03cd5c846f05aad24bb27add2de41c3a5cbad4d4
achingachris <achinga.chris@gmail.com>
2023-03-04 12:13:18 +0300

    footer update

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -8,27 +8,27 @@ const Footer = () => {
           <div className='row'>
             {/* Footer Location*/}
             <div className='col-lg-4 mb-5 mb-lg-0'>
-              <h4 className='text-uppercase mb-4'>Location</h4>
+              <h4 className='text-uppercase mb-4'>Postal Address</h4>
               <p className='lead mb-0'>
-                2215 John Daniel Drive
+                P.O BOX 254740428522
                 <br />
-                Clark, MO 65243
+                80100 - MOMBASA
               </p>
             </div>
             {/* Footer Social Icons*/}
             <div className='col-lg-4 mb-5 mb-lg-0'>
-              <h4 className='text-uppercase mb-4'>Around the Web</h4>
-              <a className='btn btn-outline-light btn-social mx-1' href='#!'>
+              <h4 className='text-uppercase mb-4'>Around the Internet</h4>
+              <a className='btn btn-outline-light btn-social mx-1' href='#'>
                 <i className='fab fa-fw fa-facebook-f' />
               </a>
-              <a className='btn btn-outline-light btn-social mx-1' href='#!'>
+              <a className='btn btn-outline-light btn-social mx-1' href='#'>
                 <i className='fab fa-fw fa-twitter' />
               </a>
-              <a className='btn btn-outline-light btn-social mx-1' href='#!'>
+              <a className='btn btn-outline-light btn-social mx-1' href='#'>
                 <i className='fab fa-fw fa-linkedin-in' />
               </a>
-              <a className='btn btn-outline-light btn-social mx-1' href='#!'>
-                <i className='fab fa-fw fa-dribbble' />
+              <a className='btn btn-outline-light btn-social mx-1' href='#'>
+                <i className='fab fa-fw fa-github' />
               </a>
             </div>
             {/* Footer About Text*/}
