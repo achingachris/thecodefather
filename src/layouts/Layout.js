b8337b8b9d4d4e9cc8f1c71ca50004461fbd6168
doghot <achinga.chris@gmail.com>
2023-05-13 23:19:33 +0300

    updated layout: testing home page

---
+++ src/layouts/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Layout
+
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,8 @@ import CodeFather from '@/layouts/CodeFather'
 
 export default function App({ Component, pageProps }) {
   return (
-    <CodeFather>
+    <>
       <Component {...pageProps} />
-    </CodeFather>
+    </>
   )
 }
