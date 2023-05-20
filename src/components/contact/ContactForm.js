199134af6bf71418a06742b999aa64d80c078bbe
doghot <achinga.chris@gmail.com>
2023-05-20 18:04:02 +0300

    SEO

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,20 +1,21 @@
 const ContactForm = () => {
   return (
     <>
-      <form id='contactForm' data-sb-form-api-token='API_TOKEN'>
+      <form
+        id='contactForm'
+        method='post'
+        action='https://formspree.io/f/xwkjzgjl'
+      >
         {/* Name input*/}
         <div className='form-floating mb-3'>
           <input
             className='form-control'
             id='name'
+            name='name'
             type='text'
             placeholder='Enter your name...'
-            data-sb-validations='required'
           />
           <label htmlFor='name'>Full name</label>
-          <div className='invalid-feedback' data-sb-feedback='name:required'>
-            A name is required.
-          </div>
         </div>
         {/* Email address input*/}
         <div className='form-floating mb-3'>
@@ -22,30 +23,10 @@ const ContactForm = () => {
             className='form-control'
             id='email'
             type='email'
+            name='email'
             placeholder='name@example.com'
-            data-sb-validations='required,email'
           />
           <label htmlFor='email'>Email address</label>
-          <div className='invalid-feedback' data-sb-feedback='email:required'>
-            An email is required.
-          </div>
-          <div className='invalid-feedback' data-sb-feedback='email:email'>
-            Email is not valid.
-          </div>
-        </div>
-        {/* Phone number input*/}
-        <div className='form-floating mb-3'>
-          <input
-            className='form-control'
-            id='phone'
-            type='tel'
-            placeholder='(123) 456-7890'
-            data-sb-validations='required'
-          />
-          <label htmlFor='phone'>Phone number</label>
-          <div className='invalid-feedback' data-sb-feedback='phone:required'>
-            A phone number is required.
-          </div>
         </div>
         {/* Message input*/}
         <div className='form-floating mb-3'>
@@ -55,41 +36,16 @@ const ContactForm = () => {
             type='text'
             placeholder='Enter your message here...'
             style={{ height: '10rem' }}
-            data-sb-validations='required'
+            name='message'
             defaultValue={''}
           />
           <label htmlFor='message'>Message</label>
-          <div className='invalid-feedback' data-sb-feedback='message:required'>
-            A message is required.
-          </div>
         </div>
-        {/* Submit success message*/}
-        {/**/}
-        {/* This is what your users will see when the form*/}
-        {/* has successfully submitted*/}
-        <div className='d-none' id='submitSuccessMessage'>
-          <div className='text-center mb-3'>
-            <div className='fw-bolder'>Form submission successful!</div>
-            To activate this form, sign up at
-            <br />
-            <a href='https://startbootstrap.com/solution/contact-forms'>
-              https://startbootstrap.com/solution/contact-forms
-            </a>
-          </div>
-        </div>
-        {/* Submit error message*/}
-        {/**/}
-        {/* This is what your users will see when there is*/}
-        {/* an error submitting the form*/}
-        <div className='d-none' id='submitErrorMessage'>
-          <div className='text-center text-danger mb-3'>
-            Error sending message!
-          </div>
-        </div>
+
         {/* Submit Button*/}
         <div className='d-grid'>
           <button
-            className='btn btn-primary btn-lg disabled'
+            className='btn btn-primary btn-lg'
             id='submitButton'
             type='submit'
           >
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -7,6 +7,67 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <title>Chris Achinga | the codeFather</title>
+        <meta
+          name='description'
+          content='Chris Ndanyi Achinga: Experienced Fullstack Developer specializing in NodeJs, React, Next.Js, Python Django, and Angular.'
+        />
+        <meta name='title' content='Chris Achinga | the codeFather' />
+        <meta
+          name='description'
+          content='Chris Ndanyi Achinga: Experienced Fullstack Developer specializing in NodeJs, React, Next.Js, Python Django, and Angular.'
+        />
+        {/* Open Graph / Facebook */}
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://www.chrisdevcode.xyz/' />
+        <meta property='og:title' content='Chris Achinga | the codeFather' />
+        <meta
+          property='og:description'
+          content='Chris Ndanyi Achinga: Experienced Fullstack Developer specializing in NodeJs, React, Next.Js, Python Django, and Angular.'
+        />
+        <meta
+          property='og:image'
+          content='https://res.cloudinary.com/chrisdevcode/image/upload/v1684594840/DSC_7787_bxsbx6.jpg'
+        />
+        {/* Twitter */}
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://www.chrisdevcode.xyz/' />
+        <meta
+          property='twitter:title'
+          content='Chris Achinga | the codeFather'
+        />
+        <meta
+          property='twitter:description'
+          content='Chris Ndanyi Achinga: Experienced Fullstack Developer specializing in NodeJs, React, Next.Js, Python Django, and Angular.'
+        />
+        <meta
+          property='twitter:image'
+          content='https://res.cloudinary.com/chrisdevcode/image/upload/v1684594840/DSC_7787_bxsbx6.jpg'
+        />
+        <meta
+          name='keywords'
+          content='Chris Ndanyi Achinga, Fullstack Developer, Software Engineer, NodeJs, React, Next.Js, Python Django, Angular'
+        />
+        <meta name='robots' content='index,follow' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link rel='canonical' href='https://www.chrisdevcode.xyz/' />
+        <link
+          rel='alternate'
+          hrefLang='en-us'
+          href='https://www.chrisdevcode.xyz/'
+        />
+        <meta
+          property='og:title'
+          content='Chris Ndanyi Achinga: Fullstack Developer'
+        />
+        <meta
+          property='og:description'
+          content='Ambitious and versatile software engineer passionate about full-stack development and technical writing.'
+        />
+        <meta
+          property='og:image'
+          content='https://res.cloudinary.com/chrisdevcode/image/upload/v1684594840/DSC_7787_bxsbx6.jpg'
+        />
+        <meta name='twitter:card' content='summary_large_image' />
       </Head>
       <div className='d-flex flex-column h-100'>
         <Navigator />
